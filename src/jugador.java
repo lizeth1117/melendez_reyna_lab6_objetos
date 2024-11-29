179d97158fef647b997ab235e2eea3dde20e6f97
lizeth1117 <reynamlndz@hotmail.com>
2024-11-29 11:48:10 -0600

    hola

---
+++ src/jugador.java
@@ -0,0 +1,60 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lizet
+ */
+public class jugador {
+    
+    String nombre, username, password;
+    int puntos;
+    
+    
+
+    public jugador(String nombre, String username, String password, int puntos) {
+        this.nombre = nombre;
+        this.username = username;
+        this.password = password;
+        this.puntos = puntos;
+    }
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+    
+}
